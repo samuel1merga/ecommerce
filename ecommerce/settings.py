8f5a4b9767619157ea075dd50dec4c2b78f960a6
Samuel Merga <samuel1merga1@gmail.com>
2025-06-14 20:10:52 +0300

    Setup for Render deployment

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -78,16 +78,28 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
+import os
 import dj_database_url
+from pathlib import Path
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default='sqlite:///' + str(BASE_DIR / 'db.sqlite3'),
-        conn_max_age=600,
-        ssl_require=True
-    )
-}
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+DATABASE_URL = os.getenv('DATABASE_URL')
+
+if DATABASE_URL:
+    DATABASES = {
+        'default': dj_database_url.parse(DATABASE_URL, conn_max_age=600, ssl_require=True)
+    }
+else:
+    # fallback to local SQLite for development
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+
+
 
 
 
